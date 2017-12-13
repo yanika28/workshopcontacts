9fb71d8ca45f763cad4d74f387714c4e052a9b3d
donlawat <58160391@go.buu.ac.th>
2017-12-13 18:35:18 +0700

    testwo

--- test/contactlist.spec.js
+++ test/contactlist.spec.js
@@ -20,3 +20,20 @@ describe('GET /contacts',()=>{
     })
   })
   })
+
+  describe('GET /contacts',()=>{
+    it('Get id:1 successfuly return status 200 ',(done)=>{
+      request(router).get('/contacts/5')
+      .expect(200)
+      .then((res)=>{
+        let contact = res.body
+        expect(contact.id).toBe(5)
+        expect(contact.name).toBe('Jora Mormont')
+        expect(contact.email).toBe('khaleesifan100@gmail.com')
+        expect(contact.phone).toBe('123-456-7890')
+        expect(contact.url).toBe('www.google.com')
+        expect(contact.notes).toBe('Lost in the friend-zone.')
+        done()
+      })
+    })
+    })
